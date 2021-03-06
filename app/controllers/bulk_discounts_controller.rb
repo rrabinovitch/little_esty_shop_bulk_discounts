a4747ecd446d3ab58ada4a3a6399d27459904094
Arika Turing <ablenker@gmail.com>
2021-03-06 14:36:42 -0700

    Add passing code for user story three feature test

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,15 +6,25 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-    
+  end
+
+  def create
+    BulkDiscount.create!(name: params[:name],
+                         discount: params[:discount],
+                         threshold: params[:discount],
+                         id: find_new_id, merchant: @merchant)
+    redirect_to merchant_bulk_discounts_path(@merchant.id)
   end
 
   private
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_new_id
+    BulkDiscount.last.id + 1
   end
 end
