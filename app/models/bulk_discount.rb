6a4ddb3560aecb2d4e50db157b28c9a27d1390f3
Arika Turing <ablenker@gmail.com>
2021-03-08 14:12:00 -0700

    Add passing code for applied discounts links

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,6 +3,8 @@ class BulkDiscount < ApplicationRecord
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices 
 
   def discount_to_percentage
     (self.discount * 100).round
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,11 +2,8 @@ class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
                         :result
-  enum result: [:failed, :success]
-
   belongs_to :invoice
   has_many :customers, through: :invoice
-  has_many :invoice_items, through: :invoice
-  has_many :items, through: :invoice_items
-  has_many :merchants, through: :items 
+
+  enum result: [:failed, :success]
 end
