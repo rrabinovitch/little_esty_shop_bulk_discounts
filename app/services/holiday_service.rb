43f7d04d512b2b9976e40f8588f2f248848ab150
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-11 00:45:14 -0700

    Service returns full JSON response without filtering - this will be taken care of by facade

--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,9 +1,9 @@
 require './app/poros/holiday'
 
 class HolidayService < ApiService
-  def self.next_three_info
+  def self.holidays
     endpoint = 'https://date.nager.at/Api/v2/NextPublicHolidays/US'
-    json = get_data(endpoint)[0..2]
-    json_data = Holiday.new(json).next_three_holidays
+    get_data(endpoint)
+    # json_data = Holiday.new(json).next_three_holidays
   end
 end
