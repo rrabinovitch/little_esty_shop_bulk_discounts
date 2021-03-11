7ae8d81305bec7565db00dc5a1d1d966cabac4f3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-11 00:45:51 -0700

    Update #initialize to reflect that Holiday PORO represents a single holiday

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -1,14 +1,15 @@
 class Holiday
-  def initialize(json_data)
-    @data = json_data
+  def initialize(holiday_data)
+    @name = holiday_data[:name]
+    @date = holiday_data[:date]
   end
 
-  def next_three_holidays
-    holidays = ""
-
-    @data.each do |holiday|
-      holidays += "#{holiday[:name]}, #{holiday[:date]}"
-    end
-    holidays = holidays[0..-2]
-  end
+  # def next_three_holidays
+  #   holidays = ""
+  #
+  #   @data.each do |holiday|
+  #     holidays += "#{holiday[:name]}, #{holiday[:date]}"
+  #   end
+  #   holidays = holidays[0..-2]
+  # end
 end
