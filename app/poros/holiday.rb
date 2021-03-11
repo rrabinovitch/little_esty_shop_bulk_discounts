e1a5c901f37922c6f96dd985ac3ca1c62a265dc9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-11 00:47:41 -0700

    Remove commented out code

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -1,17 +1,8 @@
 class Holiday
   attr_reader :name, :date
-  
+
   def initialize(holiday_data)
     @name = holiday_data[:name]
     @date = holiday_data[:date]
   end
-
-  # def next_three_holidays
-  #   holidays = ""
-  #
-  #   @data.each do |holiday|
-  #     holidays += "#{holiday[:name]}, #{holiday[:date]}"
-  #   end
-  #   holidays = holidays[0..-2]
-  # end
 end
