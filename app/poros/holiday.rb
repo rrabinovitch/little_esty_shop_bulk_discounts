7f5b8ae410571ede3878cc3eee836340ce25fbb1
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-11 01:04:06 -0700

    Add data flow annotations

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -1,4 +1,7 @@
 class Holiday
+  # before refactor, the Holiday PORO represented a collection of three holidays and the #next_three_holidays method returned the data we needed about all three holidays in a single string
+  # now each Holiday object represents a single holiday with attributes that can be read by the view
+
   attr_reader :name, :date
 
   def initialize(holiday_data)
--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,5 +1,6 @@
 class HolidayService < ApiService
   def self.holidays
+    # grabs and returns the raw JSON response from the API endpoint
     endpoint = 'https://date.nager.at/Api/v2/NextPublicHolidays/US'
     get_data(endpoint)
   end
