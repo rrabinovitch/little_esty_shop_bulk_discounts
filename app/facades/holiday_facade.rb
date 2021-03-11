6dc38794a04879d2039c20652ebd2abfcbbd377c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-11 00:43:49 -0700

    Remove #initialize and call on service from ::get_holidays instead

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -1,9 +1,12 @@
 class HolidayFacade
-  def initialize
-    @holiday_service = HolidayService.new
+  def self.next_three_holidays
+    next_three_holidays_data = get_holidays_data[0..2]
+    next_three_holidays = next_three_holidays_data.map do |holiday_data|
+      Holiday.new(holiday_data)
+    end
   end
 
-  def self.get_next_three_holidays
-    binding.pry
+  def self.get_holidays_data
+    holidays_data = HolidayService.holidays
   end
 end
